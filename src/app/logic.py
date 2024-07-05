b3b64319b09096b1b1e1d88565377aa90bca1317
LebedevPavel2 <lebedevpavel2@yandex.ru>
2024-07-05 14:13:29 +0000

    В файле logic.py внедрён класс TransactionType, а также добавлено несколько проверок на тип аргумента

--- src/app/logic.py
+++ src/app/logic.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from app.models import Transaction, TransactionReport
+from app.models import Transaction, TransactionReport, TransactionType
 
 transactions: dict[int, list[Transaction]] = {}
 transaction_reports: list[TransactionReport] = []
@@ -11,9 +11,17 @@ class TransactionService:
 
     @staticmethod
     def create_transaction(
-        user_id: int, amount: float, transaction_type: str,
+        user_id: int,
+        amount: float | int,
+        transaction_type: TransactionType | str,
     ) -> None:
         """Создание новой транзакции."""
+        if not isinstance(user_id, int):
+            raise TypeError('user_id must be int')
+
+        if isinstance(transaction_type, str):
+            transaction_type = TransactionType(transaction_type)
+
         transaction = Transaction(amount, transaction_type, datetime.now())
         if user_id not in transactions:
             transactions[user_id] = [transaction]
@@ -28,22 +36,35 @@ class TransactionService:
         user_transactions: list[Transaction],
     ) -> None:
         """Сохранение отчета о транзакциях."""
-        transaction_reports.append(TransactionReport(
-            user_id, date_start, date_end, user_transactions,
-        ))
+        transaction_reports.append(
+            TransactionReport(
+                user_id,
+                date_start,
+                date_end,
+                user_transactions,
+            )
+        )
 
     @staticmethod
     def get_transactions(
-        user_id, date_start: datetime, date_end: datetime,
+        user_id,
+        date_start: datetime,
+        date_end: datetime,
     ) -> list[Transaction]:
         """Получение списка транзакций."""
+        if not isinstance(date_start, datetime) or not isinstance(date_end, datetime):  # noqa: E501
+            raise TypeError('date_start and date_end must be datetime')
+
         user_transactions = []
         for transaction in transactions[user_id]:
             if date_start < transaction.date < date_end:
                 user_transactions.append(transaction)
 
         TransactionService.save_report(
-            user_id, date_start, date_end, user_transactions,
+            user_id,
+            date_start,
+            date_end,
+            user_transactions,
         )
 
         return user_transactions
