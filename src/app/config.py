790f5756fd0ba28d965db24fa7033e04d25a3694
LebedevPavel2 <lebedevpavel2@yandex.ru>
2024-08-19 23:54:53 +0500

    перемименована переменна отвечающая за название bd

--- src/app/config.py
+++ src/app/config.py
@@ -9,8 +9,8 @@ class Settings(BaseSettings):
     db_password: str = 'postgres'
     db_host: str = 'host.docker.internal'
     db_port: str = '5432'
-    db_table: str = 'credit_card'
-    db_url: str = f'postgresql+asyncpg://{db_user}:{db_password}@{db_host}:{db_port}/{db_table}'  # noqa: E501, WPS221
+    db_name: str = 'credit_card'
+    db_url: str = f'postgresql+asyncpg://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}'  # noqa: E501, WPS221
     db_echo: bool = True
 
 
